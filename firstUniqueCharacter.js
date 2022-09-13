b5657d8a175c09ad4ba5f479e51f8ef981c68519
ribrahimovic <ramiz@sqills.online>
2022-09-13 13:51:55 -0500

    find first unique value

--- firstUniqueCharacter.js
+++ firstUniqueCharacter.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+function findUnique(string) {
+    let hash = {};
+    string.split("").forEach(element => (hash.hasOwnProperty(element) ? hash[element] += 1 : hash[element] = 1));
+    return string.split("").indexOf(string.split("").find(element => hash[element] == 1))
+}
+
+console.log(findUnique("heleloho"))
