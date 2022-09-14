7b838ba0e2d3b78fd513c81fe352b7d5438c0f11
ribrahimovic <ramiz@sqills.online>
2022-09-13 19:29:27 -0500

    space reduction change

--- palindromeCheck.js
+++ palindromeCheck.js
@@ -2,4 +2,4 @@ function paliCheck(string) {
     return (string.replace(/[^\w]/gi, '').toLowerCase() == string.replace(/[^\w]/gi, '').toLowerCase().split("").reverse().join(""));
 }
 
-console.log(paliCheck(".rad.ada .R"))
+console.log(paliCheck(".rad.ada .R"));
