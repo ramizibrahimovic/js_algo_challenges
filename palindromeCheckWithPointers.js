d11bba3962a7863c745a0dd78e64f614237389db
ribrahimovic <ramiz@sqills.online>
2022-09-13 19:30:52 -0500

    space save change

--- palindromeCheckWithPointers.js
+++ palindromeCheckWithPointers.js
@@ -1,9 +1,9 @@
 function paliCheck(str) {
-    let string = str.toLowerCase().replace(/[^\w]/gi, '');
+    str = str.toLowerCase().replace(/[^\w]/gi, '');
     let p1 = 0;
-    let p2 = string.length -1;
+    let p2 = str.length -1;
     while (p1++ <= p2--) {
-        if (string[p1] != string[p2]) return false;
+        if (str[p1] != str[p2]) return false;
     }
     return true;
 }
